c75e02277642b38a91d24646f545470c42a6b322
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2018-09-05 13:24:12 -0600

    create dock and attr tests

---
+++ test/dock_test.rb
@@ -0,0 +1,24 @@
+require 'pry'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+require './lib/dock'
+
+
+class DockTest < Minitest::Test
+  def setup
+  end
+
+  def test_it_exists
+    dock = Dock.new("The Rowing Dock", 3)
+    assert_instance_of Dock, dock
+  end
+
+  def test_attributes
+    dock = Dock.new("The Rowing Dock", 3)
+    assert_equal "The Rowing Dock", dock.name
+    assert_equal 3, dock.max_rental_time
+  end
+
+
+end 
