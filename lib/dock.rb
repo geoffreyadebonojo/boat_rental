7be850ceb37e38059f5f766162577a78a730e5e9
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2018-09-05 13:54:14 -0600

    create rent and log hour method and test

--- lib/dock.rb
+++ lib/dock.rb
@@ -1,11 +1,27 @@
 class Dock
   attr_reader :name,
-              :max_rental_time
-
+              :max_rental_time,
+              :rented_boats,
+              :renters
+              
   def initialize(name, max_rental_time)
     @name = name
     @max_rental_time = max_rental_time
+    @rented_boats = []
+    @revenue = 0
+    @renters = []
   end 
+
+  def rent(boat, renter)
+    @rented_boats <<  boat
+    @renters << renter
+  end
+
+  def log_hour
+    @rented_boats.each do |boat|
+      @revenue += boat.price_per_hour
+    end
+  end
 
 
 end
--- test/dock_test.rb
+++ test/dock_test.rb
@@ -3,7 +3,8 @@ require 'minitest/autorun'
 require 'minitest/pride'
 
 require './lib/dock'
-
+require './lib/boat'
+require './lib/renter'
 
 class DockTest < Minitest::Test
   def setup
@@ -20,5 +21,23 @@ class DockTest < Minitest::Test
     assert_equal 3, dock.max_rental_time
   end
 
+  def test_it_can_rent
+    dock = Dock.new("The Rowing Dock", 3)
+    kayak_1 = Boat.new(:kayak, 20)
+    kayak_2 = Boat.new(:kayak, 20)    
+    canoe = Boat.new(:canoe, 25)    
+    sup_1 = Boat.new(:standup_paddle_board, 15)    
+    sup_2 = Boat.new(:standup_paddle_board, 15)    
+    patrick = Renter.new("Patrick Star", "4242424242424242")
+    eugene = Renter.new("Eugene Crabs", "1313131313131313")    
+    
+    dock.rent(kayak_1, patrick)
+    dock.rent(kayak_2, patrick)
+    
+    dock.log_hour
+
+    assert_equal [kayak_1, kayak_2], dock.rented_boats
+    assert_equal [patrick, patrick], dock.renters
+  end
 
 end 
